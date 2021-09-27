66461ef5b930a0d7c856cc875c87c6f21f811fb6
NagaRaju <47264640+nagarajub22@users.noreply.github.com>
2021-09-27 15:41:21 +0000

    Update helper

--- src/components/App.component.tsx
+++ src/components/App.component.tsx
@@ -1,25 +1,40 @@
-import React from "react";
-import styles from './App.component.scss';
+import { GizmoHelper, GizmoViewport, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React, { useRef } from "react";
 
 interface Props {
   name:
   string
 }
 
-class App extends React.Component<Props> {
-  render() {
+function App(props: Props) {
 
-    return (
-      <>
-        <h1 className={styles.h1}>
-          Hello 
-        </h1>
-        <button type="button">
-          This is a button
-        </button>
-      </>
-    );
-  }
+  const controlRef = useRef<any>();
+
+  return (
+    <Canvas
+      camera={{ fov: 50, near: 1, far: 100, aspect: window.innerWidth / window.innerHeight, position: [0, 0, 7] }}
+    >
+      <gridHelper />
+      {/* <CGizmoHelper/> */}
+
+
+      <GizmoHelper
+        alignment="top-right" // widget alignment within scene
+        onTarget={() => controlRef?.current?.target}
+        onUpdate={() => controlRef.current?.update()}
+      >
+        <GizmoViewport axisColors={['red', 'green', 'blue']} labelColor="black" />
+      </GizmoHelper>
+      <OrbitControls ref={controlRef} />
+
+      <mesh position={[0, 0, 0]} >
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial />
+      </mesh>
+    </Canvas>
+  )
+
 }
 
 export default App;
